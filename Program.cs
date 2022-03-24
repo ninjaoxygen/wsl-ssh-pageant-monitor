27375d5de728871ae0a685866fc25ae69d7417dc
Chris Poole <chris@avsp.co.uk>
2022-03-24 12:16:45 +0000

    Initial revision

---
+++ Program.cs
@@ -0,0 +1,88 @@
+﻿using System.Runtime.InteropServices;
+
+[DllImport("kernel32.dll", SetLastError = true)]
+[return: MarshalAs(UnmanagedType.Bool)]
+static extern bool AllocConsole();
+
+AllocConsole();
+
+if (args.Length < 3)
+{
+    Console.Error.WriteLine("wsl-ssh-pageant-monitor.exe sock executable arguments");
+    Console.Error.WriteLine("press any key to exit...");
+    Console.ReadKey();
+    return 1;
+}
+
+bool exiting = false;
+
+AppDomain.CurrentDomain.DomainUnload += (object? sender, EventArgs _) =>
+{
+    Console.WriteLine("exiting because DomainUnload");
+    exiting = true;
+};
+
+Console.CancelKeyPress += delegate
+{
+    Console.WriteLine("exiting because CancelKeyPress");
+    exiting = true;
+};
+
+string sockPath = args[0];
+string executable = args[1];
+string arguments = args[2];
+
+Console.WriteLine($"using sockPath {sockPath}");
+Console.WriteLine($"using executable {executable}");
+Console.WriteLine($"using arguments {arguments}");
+
+if (!sockPath.EndsWith(".sock"))
+{
+    Console.Error.WriteLine("sock path does not end with .sock, exiting so we do not accidentally delete anything important");
+    return 3;
+}
+
+void MonitorProcess()
+{
+    while (!exiting)
+    {
+
+        Console.WriteLine("starting process...");
+
+        if (File.Exists(sockPath))
+        {
+            Console.WriteLine("sock file already exists, attempting to delete...");
+            try
+            {
+                File.Delete(sockPath);
+            }
+            catch (Exception ex)
+            {
+                Console.Error.WriteLine($"failed to delete sock file: {ex.Message}");
+            }
+        }
+        else
+        {
+            Console.WriteLine("sock file does not exist, continuing");
+        }
+
+        System.Diagnostics.Process prc = new System.Diagnostics.Process();
+        prc.StartInfo.FileName = executable;
+        prc.StartInfo.Arguments = arguments;
+        prc.Start();
+
+        Console.WriteLine("waiting for exit...");
+        prc.WaitForExit();
+
+        if (!exiting)
+        {
+            Console.WriteLine("exited, sleeping...");
+            System.Threading.Thread.Sleep(5000);
+        }
+    }
+
+}
+
+MonitorProcess();
+
+return 0;
---
+++ run.ps1
@@ -0,0 +1,1 @@
+.\bin\Release\net6.0\wsl-ssh-pageant-monitor.exe C:\ssh-agent.sock ${env:USERPROFILE}\scoop\apps\wsl-ssh-pageant\current\wsl-ssh-pageant-gui.exe "--systray --winssh ssh-pageant --wsl C:\ssh-agent.sock"
